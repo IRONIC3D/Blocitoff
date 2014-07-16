83be4c9c9aeff0fafb28f690268d1efd44aab1e8
Iyad Horani <i3d@ironic3d.com>
2014-07-16 06:48:07 +0000

    Amended the environment/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Adding Devise specific configs
-  config.action_mailer.default_url_options = { host: 'iyad-blocitoff.herokuapp.com'}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
+  # bloccit email
+  config.action_mailer.default_url_options = { host: 'iyad-blocitoff.herokuapp.com' }
 end
